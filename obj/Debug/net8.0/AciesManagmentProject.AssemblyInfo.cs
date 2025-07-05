8f246e488f3da72a5f21a90e7007ae4ce1b95f91
Your Name <mostafaomeromer@gmail.com>
2025-07-05 19:53:14 +0300

    lastUpdate

--- obj/Debug/net8.0/AciesManagmentProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/AciesManagmentProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AciesManagmentProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0b6377b2cb129400e580addfa501f44ae2e9c95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7baaf0fc5fdfe318a8002d30a83f825e8f488920")]
 [assembly: System.Reflection.AssemblyProductAttribute("AciesManagmentProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AciesManagmentProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
